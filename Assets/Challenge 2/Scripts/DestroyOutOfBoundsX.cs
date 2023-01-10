b619fd77b2d8eef8def1c055adc60e0763f3ccd8
Joel <120654417+JoelDesigner3D@users.noreply.github.com>
2023-01-10 15:02:00 +0100

    Add collider on ground

--- Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,29 +4,25 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
-    private float bottomLimit = -5;
-    private float xMax = -40f;
-    private float xMin = 20f;
+    private float leftLimit = -43f;
+    private float bottomLimit = -5f;
 
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
+        if (transform.position.x < leftLimit)
         {
             Destroy(gameObject);
         } 
+
         // Destroy balls if y position is less than bottomLimit
-        else if (transform.position.z < bottomLimit)
+        if (transform.position.y < bottomLimit)
         {
-            Destroy(gameObject);
-        }
-        else if (transform.position.x < xMax)
-        {
-            Destroy(gameObject);
+           Destroy(gameObject);
         }
 
     }
+   
 }
