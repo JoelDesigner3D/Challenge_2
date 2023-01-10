1e26828e9c3a10851558df7b5442710426e48176
Joel <120654417+JoelDesigner3D@users.noreply.github.com>
2023-01-10 16:13:39 +0100

    Delay
    Only allow the player to spawn a new dog after a certain amount of time has passed

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +7,35 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    Boolean pause = false;
+
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            //Only allow the player to spawn a new dog after a certain amount of time has passed
+            if (!pause) {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                StartCoroutine(ExampleCoroutine());
+            }
+            
+            
         }
     }
+
+    
+
+
+IEnumerator ExampleCoroutine()
+{
+        pause = true;
+
+        //yield on a new YieldInstruction that waits for 1 seconds.
+        yield return new WaitForSeconds(1);
+
+        pause = false;
+}
+
 }
